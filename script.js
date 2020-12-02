16babea73cd51835ac2ebee2919135726c72fc97
Jesse <moore.jesse@gmail.com>
2020-12-02 08:22:01 -0600

    Added function to check shuttle readiness and to modify the document reflecting the output

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ window.addEventListener('load', () => {
         try {
             const formData = parseInput(e.target)
             validateInput(formData)
+            checkShuttleReadiness(formData)
         } catch (error) {
             if (error === 'blankField') {
                 window.alert('All fields are required!')
@@ -24,6 +25,8 @@ window.addEventListener('load', () => {
                 window.alert(
                     'Make sure to enter valid information for each field!'
                 )
+            } else {
+                console.error(error)
             }
         }
     })
@@ -71,4 +74,38 @@ function isNumber(number) {
         return true
     }
     return false
+}
+
+function checkShuttleReadiness({ fuelLevel, cargoMass }) {
+    const fuelReady = Number(fuelLevel) >= 10000
+    const cargoReady = Number(cargoMass) <= 10000
+    const fuelStatusElement = document.querySelector('#fuelStatus')
+    const cargoStatusElement = document.querySelector('#cargoStatus')
+    const launchStatus = document.querySelector('#launchStatus')
+
+    if (fuelReady) {
+        fuelStatusElement.innerText = 'Fuel level high enough for launch'
+        fuelStatusElement.style.color = 'unset'
+    } else {
+        fuelStatusElement.innerText = 'Fuel level too low for launch'
+        fuelStatusElement.style.color = 'red'
+    }
+
+    if (cargoReady) {
+        cargoStatusElement.innerText = 'Cargo mass low enough for launch'
+        cargoStatusElement.style.color = 'unset'
+    } else {
+        cargoStatusElement.innerText = 'Cargo mass too high for launch'
+        cargoStatusElement.style.color = 'red'
+    }
+
+    if (fuelReady && cargoReady) {
+        launchStatus.innerText = 'Shuttle is ready for launch'
+        launchStatus.style.color = 'green'
+        document.querySelector('#faultyItems').style.visibility = 'hidden'
+    } else {
+        launchStatus.innerText = 'Shuttle not ready for launch'
+        launchStatus.style.color = 'red'
+        document.querySelector('#faultyItems').style.visibility = 'visible'
+    }
 }
